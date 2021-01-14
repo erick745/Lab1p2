78f017b00813a14d17a8b5f6ae227999ff24cca5
Ditmar <ditmarcastro@gmail.com>
2021-01-14 14:28:33 -0400

    first version of app

---
+++ products.js
@@ -0,0 +1,13 @@
+class Product {
+  constructor(id, name, description, quatity) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.quatity = quatity;
+  }
+}
+class ProductManagement {
+  constructor() {
+    this.listproducts = new Array();
+  }
+}
