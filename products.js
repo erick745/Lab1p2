b66605850b45b925f3b89dbcd03bc238e9c4778f
Ditmar <ditmarcastro@gmail.com>
2021-01-14 14:33:43 -0400

    products add and remove

--- products.js
+++ products.js
@@ -10,4 +10,16 @@ class ProductManagement {
   constructor() {
     this.listproducts = new Array();
   }
+  addProduct(product) {
+    this.listproducts.push(product);
+  }
+  removeProducts(product) {
+    for (var i = 0; i < this.listproducts.length; i++) {
+      if (this.listproducts[i] == product) {
+        this.listproducts.splice(i, 1);
+        return true;
+      }
+    }
+    return false;
+  }
 }
